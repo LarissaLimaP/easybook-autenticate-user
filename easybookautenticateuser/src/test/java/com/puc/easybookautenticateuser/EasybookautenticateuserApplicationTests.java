a8f0dee7fa5b159d101e976ed3f0e7890fd5cef9
larissa.lima <larissa.lima@logpyx.com>
2025-03-07 22:15:29 -0300

    First commit

---
+++ easybookautenticateuser/src/test/java/com/puc/easybookautenticateuser/EasybookautenticateuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.easybookautenticateuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybookautenticateuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
